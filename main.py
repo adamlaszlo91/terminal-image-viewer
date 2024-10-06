fc1bf076068bb81866b22dea05fb676dd8bfdc56
Ádám László <adam.laszlo.91@gmail.com>
2024-10-06 19:59:23 +0200

    Fix value mapping

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 from PIL import Image, ImageOps
 
-value_to_character_map = '█$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`\' '
+value_to_character_map = '█$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`\' '[::-1]
 
 
 def image_to_text(image: np.ndarray) -> str:
@@ -11,7 +11,7 @@ def image_to_text(image: np.ndarray) -> str:
         for k in range(image.shape[1]):
             greyscale_value = image[i][k]
             text += value_to_character_map[int(
-                greyscale_value * (len(value_to_character_map) / 255.0))-1]
+                (len(value_to_character_map) - 1) * (greyscale_value / 255))]
         text += '\n'
     return text
 
